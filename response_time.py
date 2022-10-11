fcbbaa3f01c6682979f4924c4971e0cd0ee325a9
Badr Kamel <36001332+badrkamel@users.noreply.github.com>
2022-10-11 21:17:46 +0200

    Update response_time.py

--- response_time.py
+++ response_time.py
@@ -2,5 +2,10 @@ import requests
 
 url = "https://api.binance.com/api/v1/klines"
 
-response = requests.get(url)
+params = {
+	'symbol': 'BTCUSDT',
+	'interval': '1h'
+}
+
+response = requests.get(url, params=params)
 print(response.elapsed.total_seconds())
