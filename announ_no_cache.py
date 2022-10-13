b258a0183da7c5d970e8e386e3942e57091a225d
Badr Kamel <36001332+badrkamel@users.noreply.github.com>
2022-10-13 17:34:58 +0200

    Update announ_no_cache.py

--- announ_no_cache.py
+++ announ_no_cache.py
@@ -1,11 +1,24 @@
-import requests
+import aiohttp
+from fake_headers import Headers
+from asyncio import get_event_loop
+
 
 url = "https://www.binance.me/bapi/composite/v1/public/cms/article/latest/query"
 url2 = "https://www.binance.com/bapi/composite/v1/public/cms/article/latest/query"
 
+header = Headers(
+    headers=True
+)
 
-for i in range(10):
-    response = requests.get(f'{url}?pageSize={i}')
+async def run_app():
+    for i in range(10000):
 
-    print(response.elapsed.total_seconds())
-    print(response.headers['X-Cache'])
+        async with aiohttp.ClientSession() as session:
+            async with session.get(f'{url}?pageSize={i}', headers=header.generate()) as response:
+
+                # print(response.)
+                # print(response.headers)
+                print(response.headers['X-Cache'])
+
+
+get_event_loop().run_until_complete(run_app())
